c2d7cefca5dc7c314f81671682a46aa72a03751a
michqiueli <michiqueli@gmail.com>
2023-07-11 11:07:24 -0300

    Modificaciones del Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,13 @@
 import SearchBar from "../SearchBar/SearchBar.jsx"
 import style from "./Nav.module.css"
-import { Link } from "react-router-dom"
+import { Link, useHref } from "react-router-dom"
 
 export default function Nav ({onSearch}){
     return (
         <div className={style.navBar}>
         <div className={style.container}>
-        <button className={style.about}>< Link to ='./About'>About</Link></button>
-        <button className={style.home}>< Link to ='./Home'>Home</Link></button>
+        <Link to ='./About'><button className={style.about} >About</button></Link>
+        < Link to ='./Home'><button className={style.home}>Home</button></Link>
         <SearchBar onSearch = {onSearch}></SearchBar>
         </div>
         </div>
