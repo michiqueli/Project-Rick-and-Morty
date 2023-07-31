c979877092441d5379990e033c3020ba8df704ca
michqiueli <michiqueli@gmail.com>
2023-07-31 20:01:25 -0300

    CR finalizado

--- Client/src/components/Details/Details.jsx
+++ Client/src/components/Details/Details.jsx
@@ -9,7 +9,7 @@ export default function Details(){
     const [character, setCharacter] = useState({})
 
     useEffect(() => {
-      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
       if (data.name) {
          setCharacter(data);
       } else {
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,14 +1,15 @@
-const data = require("./utils/data")
 var http = require("http");
+const getCharacterById = require ('./controllers/getCharacterBy Id')
 const PORT = 3001;
 
 const server = http.createServer((req, res) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    if (req.url.includes("/rickandmorty/character")){
-        const id = Number(req.url.split("/").slice(3));
-        character = data.find(char => char.id === id)
-              res.writeHead (200, {"Content-Type": "application/json"});
-              res.end(JSON.stringify(character));
-            }
-          }).listen(PORT, "localhost");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.url.includes("/rickandmorty/character")){
+    const id = Number(req.url.split("/").at(-1));
+    let character = getCharacterById(id)
+      res.writeHead (200, {"Content-Type": "application/json"});
+      res.end(JSON.stringify(character));
+    }
+  }).listen(PORT, "localhost");
+  
 module.exports = server;
